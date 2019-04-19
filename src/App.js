e4979e22627a79538d33b7884e32e5de91d3ece6
Elan Green <green.elan5@gmail.com>
2019-04-19 11:00:43 -0500

    almost done

--- src/App.js
+++ src/App.js
@@ -5,17 +5,12 @@ import TechSpecs from './components/TechSpecs';
 import Summary from './components/Summary';
 
 class App extends Component {
-  state = {
-     selected: {},
-     unseleceted: {}
-  }
-
-  componentDidMount() {
-    console.log(this.props.features.selected)
-    this.setState({
+  constructor(props) {
+    super(props);
+    this.state = {
       selected: this.props.features.selected,
       unselected: this.props.features.unselected
-    });
+    }
   }
 
   updateFeature(feature, newValue) {
@@ -27,50 +22,12 @@ class App extends Component {
   }
 
   render() {
-    const summary = Object.keys(this.state.selected)
-          .map(key => <div className="summary__option" key={key}>
-            <div className="summary__option__label">{key}  </div>
-            <div className="summary__option__value">{this.state.selected[key].name}</div>
-            <div className="summary__option__cost">
-              { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                  .format(this.state.selected[key].cost) }
-            </div>
-        </div>)
-
-    const total = Object.keys(this.state.selected)
-          .reduce((acc, curr) => acc + this.state.selected[curr].cost, 0);    
-
-    console.log(this.state)
-    const features = Object.keys(this.state.unselected)
-          .map(key => {
-            const options = this.state.unselected[key].map((item, index) => {
-              const selectedClass = item.name === this.state.selected[key].name ? 'feature__selected' : '';
-              const featureClass = 'feature__option ' + selectedClass;
-              return <li key={index} className="feature__item">
-                <div className={featureClass}
-                  
-                  onClick={e => this.updateFeature(key, item)}>
-                    { item.name }
-                    ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                      .format(item.cost) })
-                </div>
-              </li>
-            });
-
-            return <div className="feature" key={key}>
-              <div className="feature__name">{key}</div>
-              <ul className="feature__list">
-                { options }
-              </ul>
-            </div>
-          });      
-
     return (
       <div className="App">
       <NavBar />
         <main>
-          <TechSpecs features={features} />
-          <Summary total={total} summary={summary}/>
+          <TechSpecs  unselected={this.state.unselected} selected={this.state.selected} updateFeature={this.updateFeature}/>
+          <Summary selected={this.state.selected}/>
         </main>
       </div>
     );
--- src/components/TechSpecs.js
+++ src/components/TechSpecs.js
@@ -2,11 +2,36 @@ import React, { Component } from 'react'
 
 export class TechSpecs extends Component {
   render() {
+    
+    const features = Object.keys(this.props.unselected)
+          .map(key => {
+            const options = this.props.unselected[key].map((item, index) => {
+              const selectedClass = item.name === this.props.selected[key].name ? 'feature__selected' : '';
+              const featureClass = 'feature__option ' + selectedClass;
+              return <li key={index} className="feature__item">
+                <div className={featureClass}
+                  
+                  onClick={e => this.props.updateFeature(key, item)}>
+                    { item.name }
+                    ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
+                      .format(item.cost) })
+                </div>
+              </li>
+            });
+
+            return <div className="feature" key={key}>
+              <div className="feature__name">{key}</div>
+              <ul className="feature__list">
+                { options }
+              </ul>
+            </div>
+          });      
+
     return (
       <div>
         <section className="main__form">
             <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
-              { this.props.features }
+              { features }
           </section>
       </div>
     )
