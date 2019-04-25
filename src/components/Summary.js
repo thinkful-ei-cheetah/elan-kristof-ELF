dadaa220f6bd677ba821a9ef0e5401a4bf78b183
Kristof-Pierre Cummings <kristofpierre8@gmail.com>
2019-04-25 16:25:47 -0500

    fixed key issue

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -6,7 +6,7 @@ export class Summary extends Component {
   render() {
 
     const summary = Object.keys(this.props.selected)
-    .map(key => <SummaryOptions id={key} selected={this.props.selected}/>)
+    .map(val => <SummaryOptions key={val} id={val} selected={this.props.selected}/>)
     
     const total = Object.keys(this.props.selected)
     .reduce((acc, curr) => acc + this.props.selected[curr].cost, 0);  
--- src/components/SummaryOptions.js
+++ src/components/SummaryOptions.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function SummaryOptions(props) {
   return (
-    <div className="summary__option" key={props.id}>
+    <div className="summary__option" id={props.id}>
       <div className="summary__option__label">{props.id}  </div>
       <div className="summary__option__value">{props.selected[props.id].name}</div>
       <div className="summary__option__cost">
